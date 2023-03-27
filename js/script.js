83bbebc565b0255be44f75ce2262c19961915b3d
Hala Tinn <hala79tinn@gmail.com>
2023-03-27 15:41:47 +0200

    Add game state

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 let mysteryNumber = 50;
 let playersGuess = 0;
+let guessesRemaining = 10;
+let guessesMade = 0;
+let gameState = "";
+let gameWon = false;
 
 //Input & output fields
 
@@ -19,14 +23,42 @@ function clickHandler() {
 }
 
 function playGame() {
+    guessesRemaining--;
+    guessesMade++;
+    gameState = " Guess: " + guessesMade + ", Remaining: " + guessesRemaining;
+
+
     //Player input interpreted as number - remember: input typed into HTML input default: string
     playersGuess = parseInt(input.value);
 
     if (playersGuess > mysteryNumber) {
-        output.innerHTML = "That's too high.";
+        output.innerHTML = "That's too high." + gameState;
+
+        //Check for end of game
+
+        if (guessesRemaining < 1) {
+            endGame();
+        }
+
     } else if (playersGuess < mysteryNumber) {
-        output.innerHTML = "That's too low.";
+        output.innerHTML = "That's too low." + gameState;
+
+        //Check for end of game
+
+        if (guessesRemaining < 1) {
+            endGame();
+        }
+
     } else if (playersGuess === mysteryNumber) {
-        output.innerHTML = "You got it!";
+        gameWon = true;
+        endGame();
+    }
+}
+
+function endGame() {
+    if (gameWon) {
+        output.innerHTML = "Yes, it's " + mysteryNumber + "!" + "<br>" + "It only took you " + guessesMade + " guesses.";
+    } else {
+        output.innerHTML = "No more guesses left!" + "<br>" + "The number was: " + mysteryNumber + ".";
     }
 }
